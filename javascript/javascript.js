d76635c740581f49e52849d02c6c245d76ee9dd2
JamesCouture <71405359+JamesCouture@users.noreply.github.com>
2020-10-24 15:27:00 -0400

    added some js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,5 @@
-
+//about page JS
+var button = document.getElemendId("yesbutton");
+button.addEventListener("click",function() {
+	alert("Visit the community page to ask questions and share with the community");
+});
