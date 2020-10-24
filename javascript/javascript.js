8fb3643b8129d669be2258dca64dfb2d538225b2
JamesCouture <71405359+JamesCouture@users.noreply.github.com>
2020-10-24 17:28:39 -0400

    fixed typo

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,9 +1,9 @@
 // JavaScript source code
 
 var addAlertMessage = function() {
-alert('Please take a look at the community page to share your question with us in the comment section!')
+alert('Please take a look at the community page to share your question with us in the comment section!');
 }
 
-var aboutbutton = document.getElemenyById("moreabout");
+var aboutbutton = document.getElementById("moreabout");
 aboutbutton.addEventListener('click', addAlertMessage);
 
