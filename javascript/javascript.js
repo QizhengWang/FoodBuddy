4e5fbc612c0594efc97286ce437a636e6b447086
JamesCouture <71405359+JamesCouture@users.noreply.github.com>
2020-10-24 16:07:38 -0400

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,4 @@
 // JavaScript source code
 
 var aboutbutton = document.getElemenyById("moreabout");
-aboutbutton.addEventListener('click',window.alert('If you have questions, please visit out community page and share with us your question in the comment section'));
+aboutbutton.addEventListener('click', alert('If you have questions, please visit out community page and share with us your question in the comment section'));
